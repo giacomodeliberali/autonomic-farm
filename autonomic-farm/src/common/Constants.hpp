94237474e8a1bb523fd286b1d56aad7b8ffca774
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2020-02-08 12:01:06 +0100

    Update with better constants

--- autonomic-farm/src/common/Constants.hpp
+++ autonomic-farm/src/common/Constants.hpp
@@ -2,16 +2,16 @@
 #define CONSTANTS_HPP
 
 // The strategy window size
-#define STRATEGY_WINDOW_SIZE 50
+#define STRATEGY_WINDOW_SIZE 30
 
 // The slope threshold
-#define SLOPE_THRESHOLD 0.02
-
-// If the slope is above this threshold, the number of workers added/removed is 4 instaed of 1
-#define DOUBLE_COMMAND_SLOPE_THRESHOLD 0.15
+#define SLOPE_THRESHOLD 0.08
 
 // If the slope is above this threshold, the number of workers added/removed is 2 instaed of 1
-#define QUAD_COMMAND_SLOPE_THRESHOLD 0.30
+#define DOUBLE_COMMAND_SLOPE_THRESHOLD 0.3
+
+// If the slope is above this threshold, the number of workers added/removed is 4 instaed of 1
+#define QUAD_COMMAND_SLOPE_THRESHOLD 0.50
 
 // The average throughput treshold wrt to the given throughput (10% of given throughput)
 #define AVERAGE_THROUGHPUT_THRESHOLD 0.1
